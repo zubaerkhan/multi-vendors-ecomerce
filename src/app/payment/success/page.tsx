335fd6e3d5563dfeef158e8c2154834167b36bec
Zubaer Khan <zubaerkhan2@gmail.com>
2026-03-27 21:36:32 +0600

    final

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,23 +1,11 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
-export const dynamic = "force-dynamic"
-
-export default function FailPage() {
-  const params = useSearchParams()
-  const status = params.get('status')
-
+export default function SuccessPage() {
   return (
     <div className="p-10 text-center">
-      <h1 className="text-2xl font-bold text-red-600">
-        Payment Success ✅
+      <h1 className="text-2xl font-bold text-green-600">
+        Payment Successful ✅
       </h1>
-
-      {status && (
-        <p className="mt-2 text-gray-600">
-          Your payment was completed.
-        </p>
-      )}
     </div>
   )
 }
